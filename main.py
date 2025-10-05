91389dba824832fff7498233101c0ac367e9ad5e
Melih Genel <mlihgenel@gmail.com>
2025-10-05 12:49:47 +0300

    add main file for example usage

--- main.py
+++ main.py
@@ -5,31 +5,44 @@ from losses import CategoricalCrossEntropy, MeanSquaredError as MSE
 from losses import BinaryCrossEntropy
 from accuracy import Accuracy_Regression, Accuracy_Categorical
 from optimizers import Adam
-
 import numpy as np
+import os 
+import cv2
 
-from nnfs.datasets import spiral_data
+np.set_printoptions(linewidth=200)
+from dataset import load_minst_dataset, create_data_mnist
 
-X, y = spiral_data(samples=1000, classes=3)
-X_test, y_test = spiral_data(samples=100, classes=3)
+# Data preproccessing 
+X, y, X_test, y_test = create_data_mnist('fashion_mnist_images')
 
+keys = np.array(range(X.shape[0]))
+np.random.shuffle(keys)
+X = X[keys]
+y = y[keys]
+
+X = X.reshape(X.shape[0], -1).astype(np.float32) / 255
+X_test = X_test.reshape(X_test.shape[0], -1).astype(np.float32) / 255
+
+# Create Model 
 model = Model()
 
-model.add(Dense(2, 64, weight_regularizer_l2=5e-4,
-                       bias_regularizer_l2=5e-4))
-
+model.add(Dense(X.shape[1], 128))
 model.add(ReLu())
-model.add(Dropout(0.1))
-model.add(Dense(64, 3))
+model.add(Dense(128, 128))
+model.add(ReLu())
+model.add(Dense(128, 10))
 model.add(Softmax())
 
-model.compile(loss=CategoricalCrossEntropy(),
-          optimizer=Adam(learning_rate=0.05, decay=5e-5),
-          accuracy=Accuracy_Categorical())
+model.compile(
+    loss=CategoricalCrossEntropy(),
+    optimizer=Adam(decay=1e-3),
+    accuracy=Accuracy_Categorical()
+)
 
 model.finalize()
 
-model.train(X, y,
-            validation_data=(X_test, y_test),
-            epochs=10000,
-            print_every=100)
+model.train(X, y, validation_data=(X_test, y_test), epochs=10, batch_size=128, print_every=100)
+
+model.evaluate(X_test, y_test)
+
+model.save('fm.model')
